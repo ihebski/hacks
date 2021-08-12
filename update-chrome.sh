732f9eb864cc0f33903259fbda764ab569e0a055
Ihebski <ihebski@users.noreply.github.com>
2021-08-12 22:59:12 +0100

    emoji

--- update-chrome.sh
+++ update-chrome.sh
@@ -7,6 +7,6 @@ chrome_current_version=$(curl https://omahaproxy.appspot.com/linux --silent)
 if [ "$chrome_installed_version" = "$chrome_current_version" ]; then
     printf "We are running the latest version of Firefox \U1F44D"
 else
-    echo "[+] Installing the new version of Google Chrome -> [ $chrome_current_version ] \U1F50E"
+    printf "[+] Installing the new version of Google Chrome -> [ $chrome_current_version ] \U1F50E"
     wget -q https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb -P /tmp && sudo dpkg -i /tmp/google-chrome-stable_current_amd64.deb
 fi
