dee67e1aabc7c17ced8bc7f061cc0072f552d933
Ihebski <ihebski@users.noreply.github.com>
2021-09-08 13:56:08 +0100

    progress bar

--- update-chrome.sh
+++ update-chrome.sh
@@ -8,5 +8,5 @@ if [ "$chrome_installed_version" = "$chrome_current_version" ]; then
     printf "We are running the latest version of Google Chrome \U1F44D"
 else
     printf "[+] Installing the new version of Google Chrome -> [ $chrome_current_version ] \U1F50E"
-    wget -q https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb -P /tmp && sudo dpkg -i /tmp/google-chrome-stable_current_amd64.deb
+    wget --show-progress -q https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb -P /tmp && sudo dpkg -i /tmp/google-chrome-stable_current_amd64.deb
 fi
