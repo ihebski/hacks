ec3797764e553fe3e3b96cdfe96fb92c67d507e5
Ihebski <ihebski@users.noreply.github.com>
2021-12-21 14:41:49 +0100

    update nuclei naabu httpx and subfinder

--- update-projectdiscovery-tools.sh
+++ update-projectdiscovery-tools.sh
@@ -1,9 +1,21 @@
 #!/bin/bash
-# Update projectdiscovery tools 
+# Update projectdiscovery tools by @ihebski
+
 #projectdiscovery tools https://github.com/projectdiscovery
 projectDiscovery=("httpx"  "nuclei"  "naabu"  "subfinder")
- 
+
 for tool in ${projectDiscovery[*]}; do
-    curl -s https://api.github.com/repos/projectdiscovery/${tool}/releases/latest | grep "browser_download_url" | cut -d '"' -f 4 | grep "linux_amd64" | wget -qi - && unzip ${tool}* && sudo mv ${tool} /usr/bin/ && ${tool} -version
+    # Get latest tools release
+    url=$(curl -s https://api.github.com/repos/projectdiscovery/${tool}/releases/latest | grep "browser_download_url" | cut -d '"' -f 4 | grep "linux_amd64")
+    # Extract versions
+    new_version=$(basename $url | grep -Eo '[0-9]+([.][0-9]+)?+([.][0-9]+)')
+    installed_version=$(${tool} -version &> /tmp/v ; grep "INF" /tmp/v | grep -Eo '[0-9]+([.][0-9]+)?+([.][0-9]+)' )
+
+    if [ "$installed_version" = "$new_version" ]; then
+        printf "\nWe are running the latest version of ${tool} \U1F44D\n"
+    else
+        printf "\n[+] Installing the new version of ${tool} -> [ $new_version ] \U1F50E\n"
+        # Download and install the new version of tools
+        wget --show-progress -q $url -P /tmp && unzip /tmp/${tool}* -d /tmp && sudo mv /tmp/${tool} /usr/bin/ && rm /tmp/${tool}* /tmp/v
+    fi
 done
- 
