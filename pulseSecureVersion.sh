99c9730944076822fa1109854340cde5588139ad
Ihebski <ihebski@users.noreply.github.com>
2022-02-04 13:27:32 +0100

    Update pulseSecureVersion.sh

--- pulseSecureVersion.sh
+++ pulseSecureVersion.sh
@@ -1,1 +1,5 @@
+# Pulse secure version <= R8
 curl -k https://IP/dana-na/nc/nc_gina_ver.txt | grep '<PARAM NAME="ProductVersion" VALUE="'
+
+# Pulse secure version > R8 (R9 >>)
+curl -k https://IP/dana-cached/hc/HostCheckerInstaller.osx -o version && strings version | grep '<string>'
