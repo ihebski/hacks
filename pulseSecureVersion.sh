4af7b14f9497e478c1d4ac49ff8c294d17308cdd
Ihebski <ihebski@users.noreply.github.com>
2022-02-04 15:23:37 +0100

    Update pulseSecureVersion.sh

--- pulseSecureVersion.sh
+++ pulseSecureVersion.sh
@@ -1,5 +1,8 @@
+# bash pulseSecureVersion.sh 127.0.0.1
+
+IP=$1
 # Pulse secure version <= R8
-curl -k https://IP/dana-na/nc/nc_gina_ver.txt | grep '<PARAM NAME="ProductVersion" VALUE="'
+curl -k https://$IP/dana-na/nc/nc_gina_ver.txt | grep '<PARAM NAME="ProductVersion" VALUE="'
 
 # Pulse secure version > R8 (R9 >>)
-curl -k https://IP/dana-cached/hc/HostCheckerInstaller.osx -o version && strings version | grep '<string>'
+curl -k https://$IP/dana-cached/hc/HostCheckerInstaller.osx -o version && strings version | grep '<string>'
